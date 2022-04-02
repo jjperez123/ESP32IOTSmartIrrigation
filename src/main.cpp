093ebf2fb7b798f7103104dd22dc446d01051802
Jochy Perez <jochyperez@Jochys-MacBook-Pro.local>
2022-04-02 14:17:54 -0400

    Added basic support for the TFT display

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include <Arduino.h>
 #include "sensor_reading.h" // this inclusion will link the two files together.
-#include "settings.h"
 #include "TFT_eSPI.h"
+#include "settings.h"
+
 
 Adafruit_BME280 bme; //I2C
 
+TFT_eSPI tft = TFT_eSPI();
+
+uint16_t bg = TFT_BLACK;
+uint16_t fg = TFT_WHITE;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN,OUTPUT);
   Serial.begin(115200);
+
+  tft.init();
+
+  tft.setRotation(1);
+  tft.setTextColor(fg,bg);
+  tft.fillScreen(bg);
+
+  tft.setCursor(0,0);
+  tft.println("Hello!");
+  tft.println("Starting BME sensor...");
+  delay(5000);
 
   bool status;
 
@@ -20,12 +36,16 @@ void setup() {
     while(1);
   }
 
+  tft.fillScreen(bg); //Clear the screen
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  refresh_reading(bme);
+  refresh_readings(&bme, &tft);
   delay(2000);
 }
+
+
 
 
--- src/sensor_reading.h
+++ src/sensor_reading.h
@@ -3,13 +3,13 @@
 #ifndef SENSOR_READING_H
 #define SENSOR_READINGS_H
 
-    #include <Arduino.h>
     #include <Adafruit_Sensor.h>
     #include <Adafruit_BME280.h>
+    #include "TFT_eSPI.h"
 
     #define SEALEVELPRESSURE_HPA (1013.25)
 
     // In h files you want to create declaration of the public functions that are available.
-    void refresh_reading(Adafruit_BME280 bme); // Declare in the header so that the compiler knows about it is called in
+    void refresh_readings(Adafruit_BME280* bme, TFT_eSPI* tft); // Declare in the header so that the compiler knows about it is called in
 
 #endif
